2fb3060a47e8abc72fea6fd539939bf0fc7497e8
j.ls <jlsrxlusa2021@gmail.com>
2021-01-14 12:28:28 -0800

    Created basic app.

---
+++ NetCore.Docker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+//Following tutorial from here:
+//  https://docs.microsoft.com/en-us/dotnet/core/docker/build-container
+
+namespace NetCore.Docker
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var counter = 0;
+            var max = (args.Length != 0) ? Convert.ToInt32(args[0]) : -1;
+            while (max == -1 || counter < max)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+            
+            Console.WriteLine("\nApplication complete - press any key to exit...");
+            _ = Console.ReadKey();
+        }
+    }
+}
